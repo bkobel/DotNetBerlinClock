18e5da1deed2b768e39e0fa6a464b5eced56687e
KB <bohdan.kobel@pl.abb.com>
2020-02-12 17:03:36 +0100

    Initial project structure

---
+++ BDD/BerlinClockFeatureSteps.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace BerlinClock
+{
+    [Binding]
+    public class TheBerlinClockSteps
+    {
+        private ITimeConverter _berlinClockConverter = new BerlinClockTimeConverter();
+        private string _inputTime;
+
+        [When(@"the time is ""(.*)""")]
+        public void WhenTheTimeIs(string time)
+        {
+            _inputTime = time;
+        }
+
+        [Then(@"the clock should look like")]
+        public void ThenTheClockShouldLookLike(string theExpectedBerlinClockOutput)
+        {
+            Assert.AreEqual(_berlinClockConverter.Convert(_inputTime), theExpectedBerlinClockOutput);
+        }
+    }
+}
---
+++ Classes/TimeConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BerlinClock
+{
+    public class BerlinClockTimeConverter : ITimeConverter
+    {
+        public string Convert(string isoTime)
+        {
+
+
+            return "";
+        }
+    }
+}
